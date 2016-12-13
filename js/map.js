11a205624108dc1658ce912e6ba3008218de0f98
bencmbrook <benjaminbrook@college.harvard.edu>
2016-12-13 17:41:24 -0500

    map working no data yet

--- js/map.js
+++ js/map.js
@@ -9,25 +9,25 @@ var path = d3.geo.path();
 
 var color = d3.scale.category20();
 
-var svg = d3.select("body").append("svg")
+var svg = d3.select("#map-area").append("svg")
   .attr("width", width)
   .attr("height", height);
 
 queue()
   .defer(d3.json, "data/us.json")
   .defer(d3.json, "data/us-state-centroids.json")
+  .defer(d3.json, "data/pred.json")
   .await(ready);
 
-function ready(error, us, centroid) {
+function ready(error, us, centroid, pred) {
   var countries = topojson.feature(us, us.objects.states).features,
-  neighbors = topojson.neighbors(us.objects.states.geometries);
+      neighbors = topojson.neighbors(us.objects.states.geometries);
 
   svg.selectAll("states")
     .data(countries)
     .enter().insert("path", ".graticule")
     .attr("class", "states")
     .attr("d", path)
-    .style("fill", function(d, i) { return color(d.color = d3.max(neighbors[i], function(n) { return countries[n].color; }) + 1 | 0); })
     .on('mouseover', function(d, i) {
       var currentState = this;
       d3.select(this).style('fill-opacity', 1);
